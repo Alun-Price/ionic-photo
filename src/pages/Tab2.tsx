9ea505276e4e32784d5ca1633abffa6e402138a3
Alun Price <alunprice089@gmail.com>
2024-04-22 20:44:54 +0100

    Added delete photo functionality

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -15,12 +15,14 @@ import {
   IonActionSheet,
 } from '@ionic/react';
 import { camera, trash, close } from 'ionicons/icons';
-import { usePhotoGallery } from '../hooks/usePhotoGallery';
-import ExploreContainer from '../components/ExploreContainer';
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
-  const { photos, takePhoto } = usePhotoGallery();
+  const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
+
   return (
     <IonPage>
       <IonHeader>
@@ -37,8 +39,11 @@ const Tab2: React.FC = () => {
         <IonGrid>
           <IonRow>
             {photos.map((photo, index) => (
-              <IonCol size='6' key={photo.filepath}>
-                <IonImg src={photo.webviewPath} />
+              <IonCol size='6' key={index}>
+                <IonImg
+                  onClick={() => setPhotoToDelete(photo)}
+                  src={photo.webviewPath}
+                />
               </IonCol>
             ))}
           </IonRow>
@@ -48,6 +53,28 @@ const Tab2: React.FC = () => {
             <IonIcon icon={camera}></IonIcon>
           </IonFabButton>
         </IonFab>
+        <IonActionSheet
+          isOpen={!!photoToDelete}
+          buttons={[
+            {
+              text: 'Delete',
+              role: 'destructive',
+              icon: trash,
+              handler: () => {
+                if (photoToDelete) {
+                  deletePhoto(photoToDelete);
+                  setPhotoToDelete(undefined);
+                }
+              },
+            },
+            {
+              text: 'Cancel',
+              icon: close,
+              role: 'cancel',
+            },
+          ]}
+          onDidDismiss={() => setPhotoToDelete(undefined)}
+        />
       </IonContent>
     </IonPage>
   );
